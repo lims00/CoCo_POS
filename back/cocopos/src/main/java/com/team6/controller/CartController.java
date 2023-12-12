910d9e4314c5ec61814773ecbf8daec364341c5e
indoor98 <98157935+indoor98@users.noreply.github.com>
2023-12-12 18:17:08 +0900

    카트에 물건 담는 함수 추가

--- back/cocopos/src/main/java/com/team6/controller/CartController.java
+++ back/cocopos/src/main/java/com/team6/controller/CartController.java
@@ -32,6 +32,9 @@ public class CartController {
     @Autowired
     PromotionRepository promotionRepository;
 
+    @Autowired
+    CartItemRepository cartItemRepository;
+
     @GetMapping("/customercart/{customerId}")
     public JSONObject customerCart(@PathVariable int customerId) {
         JSONObject json = new JSONObject();
@@ -39,7 +42,7 @@ public class CartController {
         Membership membership = membershipRepository.getByCustomerId(customerId);
         Customer customer = customerRepository.getById(customerId);
         Promotion promotion = promotionRepository.getPromotionById(1);
-
+        cartRepository.createCart(customerId);
 
 
         json.put("customerName", customer.getFirstName() + ' ' + customer.getLastName());
@@ -55,17 +58,33 @@ public class CartController {
     ProductID와 Quantity로 totalprice 및 상품 정보 돌려주는 (상품 번호, 상풍명, 수량, 단가, 가격 ) api
      */
 
+
+
+    /* POST 요청 보낼 때 customerId, productId, quantity를 보내주면,
+        1.cartItem 생성
+        2.cart에 cartItem 추가 ( cartItem이 cart를 FK로 참조하게 됨)
+        3.현재 Cart에 담긴 물건들의 총 가격을 새로 담긴 물건 가격만큼 업데이트
+        4. 리턴 (productId, productName, quantity, unitPrice, totalProductPrice, totalPrice)
+        위에서 totalPrice는 해당 물건 가격 * 총 갯수, totalPrice는 카트에 담긴 모든 물건들의 가격 전부 합친 것
+     */
     @PostMapping("/addtocart")
-    public JSONObject addToCartByIdAndQuantity(int productId, int quantity) {
+    public JSONObject addToCartByIdAndQuantity(int customerId, int productId, int quantity) {
         JSONObject json = new JSONObject(); // Json Object 생성
         Product product = productRepository.getById(productId);
         int unitPrice = product.getPrice();
+        int totalProductPrice = unitPrice * quantity;
+        Cart cart = cartRepository.getCartByCustomerId(customerId);
+        cartItemRepository.createCartItem(cart.getCartId(), customerId, quantity);
+        cartRepository.UpdateCartTotalPrice(totalProductPrice, cart.getCartId());
+
+
 
         json.put("productId", productId);
         json.put("productName", product.getPrice());
         json.put("quantity", quantity);
         json.put("unitPrice", unitPrice);
-        json.put("totalPrice", unitPrice*quantity);
+        json.put("totalProductPrice", totalProductPrice);
+        json.put("totalPrice", cart.getTotalPrice());
         return json;
     }
 
--- back/cocopos/src/main/java/com/team6/dao/CartRepository.java
+++ back/cocopos/src/main/java/com/team6/dao/CartRepository.java
@@ -4,4 +4,10 @@ import com.team6.entity.Cart;
 
 public interface CartRepository {
     boolean createCart(int customerId);
+
+
+    Cart getCartByCartId(int cartId);
+    Cart getCartByCustomerId(int customerId);
+
+    int UpdateCartTotalPrice(int cartId, int addPrice);
 }
